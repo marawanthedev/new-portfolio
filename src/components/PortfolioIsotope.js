27cac24e58c09cdbda71c4aa4ef3904b3b8c3467
marwantigerlab <marwan.mostafa@tigerlab.com>
2024-08-31 03:19:53 +0800

    update projects list

--- src/components/PortfolioIsotope.js
+++ src/components/PortfolioIsotope.js
@@ -82,19 +82,20 @@ const PortfolioIsotope = ({ noViewMore }) => {
           </a>
         </div>
         <div className="works-items works-masonry-items row">
-          <div className="works-col col-xs-12 col-sm-12 col-md-12 col-lg-12  sorting-insuretech ">
+
+        <div className="works-col col-xs-12 col-sm-12 col-md-12 col-lg-12 sorting-web3 ">
             <div
               className="works-item scrolla-element-anim-1 scroll-animate"
               data-animate="active"
             >
               <div className="image">
                 <div className="img">
-                  <Link legacyBehavior href="/work-single">
-                    <a>
+                  <Link legacyBehavior href="https://www.rareapepes.com">
+                    <a target="_blank">
                       <img
                         decoding="async"
-                        src="assets/images/work4.jpeg"
-                        alt="Sportscover"
+                        src="assets/images/projects/rareapepes/zombies.jpeg"
+                        alt="zombies"
                       />
                       <span className="overlay" />
                     </a>
@@ -102,19 +103,56 @@ const PortfolioIsotope = ({ noViewMore }) => {
                 </div>
               </div>
               <div className="desc">
-                <span className="category"> Insuretech </span>
+                <span className="category"> Web3,NFT </span>
                 <h5 className="name">
-                  <Link legacyBehavior href="/work-single">
-                    <a>Sportscover</a>
+                  <Link legacyBehavior href="https://www.rareapepes.com">
+                    <a target="_blank">RareApepes</a>
                   </Link>
                 </h5>
                 <div className="text">
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore.
+                  RareApePe is an innovative NFT platform where users can connect their blockchain wallets to mint unique NFTs, generate custom memes, and explore a vibrant digital art community. The website offers a seamless experience for NFT enthusiasts to create, share, and engage with exclusive digital assets.
                   </p>
                 </div>
-                <Link legacyBehavior href="/work-single">
+                <Link legacyBehavior href="https://www.rareapepes.com/">
+                  <a className="lnk" target="_blank">See project</a>
+                </Link>
+              </div>
+              <div
+                className="bg-img"
+                style={{
+                  backgroundImage: "url(assets/images/pat-2.png)",
+                }}
+              />
+            </div>
+          </div>
+          <div className="works-col col-xs-12 col-sm-12 col-md-12 col-lg-12  sorting-insuretech ">
+            <div
+              className="works-item scrolla-element-anim-1 scroll-animate"
+              data-animate="active"
+            >
+              <div className="image">
+                <div className="img">
+                <img
+                        decoding="async"
+                        src="assets/images/projects/complectus/summary.jpeg"
+                        alt="Sportscover"
+                      />
+                      <span className="overlay" />
+                </div>
+              </div>
+              <div className="desc">
+                <span className="category"> Insuretech </span>
+                <h5 className="name">
+                Complectus UK
+                </h5>
+                <div className="text">
+                  <p>
+                  Note: This product is not publicly available and is intended for internal use only.
+Complectus revolutionizes car insurance for Dacia, Renault, and Nissan vehicles in the UK by speeding up the policy issuance process for insurance agents. This software boosts efficiency and accuracy, enabling quicker and more streamlined service.
+                  </p>
+                </div>
+                <Link legacyBehavior href="#">
                   <a className="lnk">See project</a>
                 </Link>
               </div>
@@ -133,16 +171,11 @@ const PortfolioIsotope = ({ noViewMore }) => {
             >
               <div className="image">
                 <div className="img">
-                  <Link legacyBehavior href="/work-single">
-                    <a>
-                      <img
+                <img
                         decoding="async"
                         src="assets/images/projects/ukg-courier/ukg-payment.png"
                         alt="Quote form"
                       />
-                      <span className="overlay" />
-                    </a>
-                  </Link>
                 </div>
               </div>
               <div className="desc">
@@ -154,10 +187,11 @@ const PortfolioIsotope = ({ noViewMore }) => {
                 </h5>
                 <div className="text">
                   <p>
+                  Note: This product is not publicly available and is intended for internal use only.
                   UKG Courier’s new B2C feature lets agents create and manage delivery insurance policies with ease, while customers can enjoy seamless payments and quick access to their policy details via email.
                   </p>
                 </div>
-                <Link legacyBehavior href="/work-single">
+                <Link legacyBehavior href="#">
                   <a className="lnk">See project</a>
                 </Link>
               </div>
@@ -176,32 +210,26 @@ const PortfolioIsotope = ({ noViewMore }) => {
             >
               <div className="image">
                 <div className="img">
-                  <Link legacyBehavior href="/work-single">
-                    <a>
-                      <img
+                <img
                         decoding="async"
-                        src="assets/images/work7.jpg"
+                        src="assets/images/projects/sportscover/landing.jpeg"
                         alt="Explore"
                       />
                       <span className="overlay" />
-                    </a>
-                  </Link>
                 </div>
               </div>
               <div className="desc">
                 <span className="category"> Insuretech </span>
                 <h5 className="name">
-                  <Link legacyBehavior href="/work-single">
-                    <a>Complectus Uk</a>
-                  </Link>
+                 Sportscover
                 </h5>
                 <div className="text">
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore.
+                  Note: This product is not publicly available and is intended for internal use only.
+                  SportsCover's insurance system simplifies the process for UK agents providing coverage for athletes. The system enables agents to efficiently create insurance policies and generate invoices for customers, streamlining both policy management and billing.
                   </p>
                 </div>
-                <Link legacyBehavior href="/work-single">
+                <Link legacyBehavior href="#">
                   <a className="lnk">See project</a>
                 </Link>
               </div>
@@ -213,50 +241,7 @@ const PortfolioIsotope = ({ noViewMore }) => {
               />
             </div>
           </div>
-          <div className="works-col col-xs-12 col-sm-12 col-md-12 col-lg-12 sorting-web3 ">
-            <div
-              className="works-item scrolla-element-anim-1 scroll-animate"
-              data-animate="active"
-            >
-              <div className="image">
-                <div className="img">
-                  <Link legacyBehavior href="/work-single">
-                    <a>
-                      <img
-                        decoding="async"
-                        src="assets/images/work1.jpeg"
-                        alt="Mozar"
-                      />
-                      <span className="overlay" />
-                    </a>
-                  </Link>
-                </div>
-              </div>
-              <div className="desc">
-                <span className="category"> Web3,NFT </span>
-                <h5 className="name">
-                  <Link legacyBehavior href="/work-single">
-                    <a>RareApepes</a>
-                  </Link>
-                </h5>
-                <div className="text">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore.
-                  </p>
-                </div>
-                <Link legacyBehavior href="/work-single">
-                  <a className="lnk">See project</a>
-                </Link>
-              </div>
-              <div
-                className="bg-img"
-                style={{
-                  backgroundImage: "url(assets/images/pat-2.png)",
-                }}
-              />
-            </div>
-          </div>
+ 
           <div className="works-col col-xs-12 col-sm-12 col-md-12 col-lg-12 sorting-gifting ">
             <div
               className="works-item scrolla-element-anim-1 scroll-animate"
@@ -264,11 +249,11 @@ const PortfolioIsotope = ({ noViewMore }) => {
             >
               <div className="image">
                 <div className="img">
-                  <Link legacyBehavior href="/work-single">
-                    <a>
+                  <Link legacyBehavior href="https://admindashboard.marwan-mostafa.com">
+                    <a target="_blank">
                       <img
                         decoding="async"
-                        src="assets/images/single6.jpg"
+                        src="assets/images/projects/recrave/landing.png"
                         alt="Kana"
                       />
                       <span className="overlay" />
@@ -279,18 +264,17 @@ const PortfolioIsotope = ({ noViewMore }) => {
               <div className="desc">
                 <span className="category"> Gifting </span>
                 <h5 className="name">
-                  <Link legacyBehavior href="/work-single">
-                    <a>Recrave</a>
+                  <Link legacyBehavior href="https://admindashboard.marwan-mostafa.com">
+                    <a target="_blank">Recrave</a>
                   </Link>
                 </h5>
                 <div className="text">
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore.
+                  ReCrave's internal dashboard streamlines the management of orders, vendors, and products, ensuring smooth operations. While the public site mirrors the functionality, the dashboard is designed for internal use only, safeguarding their real data while offering a comprehensive view of business activities.
                   </p>
                 </div>
-                <Link legacyBehavior href="/work-single">
-                  <a className="lnk">See project</a>
+                <Link legacyBehavior href="https://admindashboard.marwan-mostafa.com">
+                  <a className="lnk" target="_blank">See project</a>
                 </Link>
               </div>
               <div
@@ -309,12 +293,12 @@ const PortfolioIsotope = ({ noViewMore }) => {
             >
               <div className="image">
                 <div className="img">
-                  <Link legacyBehavior href="/work-single">
-                    <a>
+                  <Link legacyBehavior href="https://trainhardeg.com/">
+                    <a target="_blank">
                       <img
                         decoding="async"
-                        src="assets/images/single6.jpg"
-                        alt="Kana"
+                        src="assets/images/projects/trainhard/products.jpeg"
+                        alt="Supplements"
                       />
                       <span className="overlay" />
                     </a>
@@ -324,18 +308,17 @@ const PortfolioIsotope = ({ noViewMore }) => {
               <div className="desc">
                 <span className="category"> E-Commerce, Supplements </span>
                 <h5 className="name">
-                  <Link legacyBehavior href="/work-single">
-                    <a>TrainHard</a>
+                  <Link legacyBehavior href="https://trainhardeg.com/">
+                    <a target="_blank">TrainHard</a>
                   </Link>
                 </h5>
                 <div className="text">
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore.
+                  TrainHard is a premier supplements store in Egypt, proudly serving customers for over three years. Their website has been a trusted destination for high-quality fitness and health supplements, offering a wide range of products to support fitness enthusiasts and athletes in their wellness journeys.
                   </p>
                 </div>
-                <Link legacyBehavior href="/work-single">
-                  <a className="lnk">See project</a>
+                <Link legacyBehavior href="https://trainhardeg.com/">
+                  <a className="lnk" target="_blank">See project</a>
                 </Link>
               </div>
               <div
