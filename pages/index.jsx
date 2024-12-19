1aaa8036a4abbaf482a221c078ee92498d4c161a
marawanthedev <tmarawan11@gmail.com>
2024-12-19 20:00:39 +0800

    update cv link

--- pages/index.jsx
+++ pages/index.jsx
@@ -96,7 +96,7 @@ const Index = () => {
                 <div className="bts">
                   <a
                     target="_blank"
-                    href="https://drive.google.com/file/d/1Y80uHsSNWTBNIKfRK-wXAbKl05VM-N7J/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1G2HmWJDikt48tdka-EZ1w7ltriEETTTY/view?usp=sharing"
                     className="btn"
                   >
                     <span>Download CV</span>
